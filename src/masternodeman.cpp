286653f1f6e677989e58e20bfc86923738344dba
MattF42 <MattF42@github.com>
2025-04-23 19:13:19 +0000

    Turn the weighting logic the right way up :)

--- src/masternodeman.cpp
+++ src/masternodeman.cpp
@@ -557,8 +557,8 @@ bool CMasternodeMan::GetNextMasternodeInQueueForPayment(int nBlockHeight, bool f
 	CMasternode::CollateralStatus err = CMasternode::GetCollateralAmount(mnInfo.vin.prevout, nCollateralAmount);
         switch(err) {
            case CMasternode::COLLATERAL_10M:
-       		    LogPrintf("CMasternode::GetNextMasternodeInQueueForPayment MN %s is 10M so %d -> %d\n",  mnpair.second.addr.ToString(), mnpair.second.GetLastPaidBlock(), mnpair.second.GetLastPaidBlock()/5);
-                    vecMasternodeLastPaid.push_back(std::make_pair(mnpair.second.GetLastPaidBlock() / 5, &mnpair.second));
+       		    LogPrintf("CMasternode::GetNextMasternodeInQueueForPayment MN %s is 10M so %d -> %d\n",  mnpair.second.addr.ToString(), mnpair.second.GetLastPaidBlock(), mnpair.second.GetLastPaidBlock() * 5);
+                    vecMasternodeLastPaid.push_back(std::make_pair(mnpair.second.GetLastPaidBlock() * 5, &mnpair.second));
                     break;
            case CMasternode::COLLATERAL_50M:
        		    LogPrintf("CMasternode::GetNextMasternodeInQueueForPayment MN %s is 50M so %d remains\n",  mnpair.second.addr.ToString(), mnpair.second.GetLastPaidBlock());
